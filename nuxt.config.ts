6a97bf79c13c8cfb1453c38a20f1ea5b6e99025e
Orielsy Diaz <orielsydiaz@gmail.com>
2026-08-30 22:06:25 -0400

    chore: remove pocket native geometry override

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  css: [
-    '~/assets/css/main.css',
-    '~/assets/css/notebook-pocket-native.css',
-  ],
+  css: ['~/assets/css/main.css'],
 
   typescript: {
     strict: true,
