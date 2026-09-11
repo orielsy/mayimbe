d2e94f65f930d348e77111e233d54c0c8c321e9f
Orielsy Diaz <orielsydiaz@gmail.com>
2026-09-10 23:19:48 -0400

    refactor: remove unused experience index composable

--- app/composables/useExperienceIndex.ts
+++
@@ -1,6 +0,0 @@
-import experienceData from '~~/generated/experience-index.json'
-import type { ExperienceIndex } from '~~/core/museum'
-
-export function useExperienceIndex(): ExperienceIndex {
-  return experienceData as ExperienceIndex
-}
